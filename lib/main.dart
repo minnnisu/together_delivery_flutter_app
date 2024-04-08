954f386d1f35cc6afbb564a5715a61c8deaf9709
CHOI-MIN-SU <mine52100@naver.com>
2024-04-08 21:02:54 +0900

    폰트 적용

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        fontFamily: 'Pretendard',
         useMaterial3: true,
       ),
       routes: routes,
