803b87064aa36a233241fa8835e1b94e0dea7b67
minnnisu <mine52100@naver.com>
2024-04-12 00:41:00 +0900

    모델 추가

---
+++ lib/post/model/postSummaryModel.dart
@@ -0,0 +1,26 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+part 'postSummaryModel.freezed.dart';
+
+part 'postSummaryModel.g.dart';
+
+@freezed
+class PostSummaryModel with _$PostSummaryModel{
+  const factory PostSummaryModel({
+    required int id,
+    required String nickname,
+    required String title,
+    required String content,
+    required String categoryCode,
+    required int deliveryFee,
+    required int minOrderFee,
+    required String createdAt,
+    required bool status,
+    required String updatedAt,
+  }) = _PostSummaryModel;
+
+  factory PostSummaryModel.fromJson(Map<String, dynamic> json) =>
+      _$PostSummaryModelFromJson(json);
+}
+
---
+++ lib/post/model/postSummaryModel.g.dart
@@ -0,0 +1,37 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'postSummaryModel.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$PostSummaryModelImpl _$$PostSummaryModelImplFromJson(
+        Map<String, dynamic> json) =>
+    _$PostSummaryModelImpl(
+      id: json['id'] as int,
+      nickname: json['nickname'] as String,
+      title: json['title'] as String,
+      content: json['content'] as String,
+      categoryCode: json['categoryCode'] as String,
+      deliveryFee: json['deliveryFee'] as int,
+      minOrderFee: json['minOrderFee'] as int,
+      createdAt: json['createdAt'] as String,
+      status: json['status'] as bool,
+      updatedAt: json['updatedAt'] as String,
+    );
+
+Map<String, dynamic> _$$PostSummaryModelImplToJson(
+        _$PostSummaryModelImpl instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'nickname': instance.nickname,
+      'title': instance.title,
+      'content': instance.content,
+      'categoryCode': instance.categoryCode,
+      'deliveryFee': instance.deliveryFee,
+      'minOrderFee': instance.minOrderFee,
+      'createdAt': instance.createdAt,
+      'status': instance.status,
+      'updatedAt': instance.updatedAt,
+    };
